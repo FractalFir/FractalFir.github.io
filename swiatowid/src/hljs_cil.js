efffc26d46ef1b7ad732f550c337d4a052602df5
Michał Kostrubiec <fractalfirdev@gmail.com>
2024-01-25 12:07:48 +0100

    Added a small snippet talking about ILVerify and LDLocA ambiguity.

--- swiatowid/src/hljs_cil.js
+++ swiatowid/src/hljs_cil.js
@@ -1,9 +1,9 @@
 function cil() {
   const regex = hljs.regex;
-     const KEYWORDS = ["locals","method","public","static","private","hidebysig"];
-   var types = ["int8","int16","int32","int64","nint","uint8","uint16","uint32","uint64","nuint","bool","char","float32","float64","void","valuetype"];
-   var short_types = ["i1","i2","i4","i8","s"];
-   var ops = ["conv","ldloc","ldc","add","stloc"];
+     const KEYWORDS = ["locals","method","public","static","private","hidebysig","assembly","class","extends","field","entrypoint"];
+   var types = ["int8","int16","int32","int64","nint","uint8","uint16","uint32","uint64","nuint","bool","char","float32","float64","void","uint","int","valuetype","native"];
+   var short_types = ["i1","i2","i4","i8","s","u1","u2","u4","u8"];
+   var ops = ["conv","ldloc","ldc","add","stloc","ret","call"];
 
    var keywords = {
     keyword: KEYWORDS,
